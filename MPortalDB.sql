8024246dd1543adea989ca6df8fcf0af3192320b
Stella <ssssss@mail.ru>
2016-11-21 05:10:09 -0800

    changed MPortalDb.xml

--- MPortalDB.sql
+++ MPortalDB.sql
@@ -1,41 +1,121 @@
-CREATE DATABASE `Mportal` /*!40100 DEFAULT CHARACTER SET latin1 */;
-CREATE TABLE `category`(
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+/*
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 5.5.49-0ubuntu0.14.04.1 : Database - MPortalDB
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`MPortalDB` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `MPortalDB`;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `parent_id` int(11) NOT NULL,
-  PRIMARY KEY (`category_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `category` */
+
+/*Table structure for table `country` */
+
+DROP TABLE IF EXISTS `country`;
+
 CREATE TABLE `country` (
-  `country_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`country_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `country` */
+
+insert  into `country`(`id`,`name`) values 
+(1,'Armenia'),
+(2,'Russia'),
+(3,'USA');
+
+/*Table structure for table `language` */
+
+DROP TABLE IF EXISTS `language`;
+
 CREATE TABLE `language` (
-  `lang_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `lang_name` varchar(3) NOT NULL,
-  PRIMARY KEY (`lang_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `language` */
+
+/*Table structure for table `mentor_category` */
+
+DROP TABLE IF EXISTS `mentor_category`;
+
 CREATE TABLE `mentor_category` (
   `user_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
   `image_url` varchar(255) NOT NULL,
-  PRIMARY KEY (`user_id`,`category_id`)
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `mentor_category_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `mentor_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mentor_category` */
+
+/*Table structure for table `mentor_status` */
+
+DROP TABLE IF EXISTS `mentor_status`;
+
 CREATE TABLE `mentor_status` (
-  `status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `status` enum('AVALIABLE','NOTAVALIABLE') NOT NULL,
-  PRIMARY KEY (`status_id`)
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `mentor_status_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mentor_status` */
+
+/*Table structure for table `request` */
+
+DROP TABLE IF EXISTS `request`;
+
 CREATE TABLE `request` (
   `from_id` int(11) NOT NULL,
   `to_id` int(11) NOT NULL,
   `accepted` tinyint(1) NOT NULL,
-  PRIMARY KEY (`from_id`,`to_id`)
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `from_id` (`from_id`),
+  KEY `to_id` (`to_id`),
+  CONSTRAINT `request_ibfk_1` FOREIGN KEY (`from_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `request_ibfk_2` FOREIGN KEY (`to_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `request` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `surname` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
@@ -43,6 +123,19 @@ CREATE TABLE `user` (
   `gender` enum('MALE','FEMALE') NOT NULL,
   `role` enum('MENTOR','MENTEE') NOT NULL,
   `country_id` int(11) NOT NULL,
-  `is_online` tinyint(1) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_online` tinyint(1) NOT NULL DEFAULT '0',
+  `dateOfBirth` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `country_id` (`country_id`),
+  CONSTRAINT `user_ibfk_3` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`name`,`surname`,`email`,`password`,`gender`,`role`,`country_id`,`is_online`,`dateOfBirth`) values 
+(2,'Stella','Tahmazyan','stella02051994@gmail.com','stella02051994','FEMALE','MENTOR',1,1,'1958-11-14');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
