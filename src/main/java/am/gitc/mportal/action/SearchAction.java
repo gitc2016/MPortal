915fd3ce7eb23da81a74bfaeb919de39f243ba4c
Ruben <ruben.sharyan@gmail.com>
2016-12-06 12:44:17 +0400

    FEAT added advance search method in search action

--- src/main/java/am/gitc/mportal/action/SearchAction.java
+++ src/main/java/am/gitc/mportal/action/SearchAction.java
@@ -1,19 +1,44 @@
 package am.gitc.mportal.action;
 
+import am.gitc.mportal.dao.impl.CategoryDaoImpl;
+import am.gitc.mportal.dao.impl.CountryDaoImpl;
+import am.gitc.mportal.dao.impl.MentorCategoryImpl;
 import am.gitc.mportal.dao.impl.UserDaoImpl;
+import am.gitc.mportal.domain.Category;
+import am.gitc.mportal.domain.Country;
+import am.gitc.mportal.domain.MentorCategory;
 import am.gitc.mportal.domain.User;
+import am.gitc.mportal.util.Global_Keys;
+import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.validation.SkipValidation;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class SearchAction extends GlobalAction {
+public class SearchAction extends GlobalAction implements ApplicationAware {
 
     private UserDaoImpl userDao;
     private String searchKeyword;
-    private  List<User> userList;
+    private List<User> userList;
+    private CountryDaoImpl countryDaoImpl;
+    private int countryId;
+    private CategoryDaoImpl categoryDao;
+    private MentorCategoryImpl mentorCategory;
+    private int categoryId;
+    private String userName;
+    private List<User> advanceSerachList;
+
+    Map<String, Object> mapApp;
+    Map<String, List<Category>> map;
 
     public SearchAction() {
         try {
             userDao = new UserDaoImpl();
+            countryDaoImpl = new CountryDaoImpl();
+            categoryDao = new CategoryDaoImpl();
+            mentorCategory = new MentorCategoryImpl();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,15 +46,57 @@ public class SearchAction extends GlobalAction {
 
     @Override
     public String execute() throws Exception {
+        userList = userDao.getSearchUserListByName(searchKeyword);
 
-         userList = userDao.getSearchUserListByName(searchKeyword);
         return SUCCESS;
     }
 
+    @SkipValidation
+    public String categoryAndSubCategory() {
+        map = new HashMap<String, List<Category>>();
+        List<Category> categoryList = categoryDao.getCategoryByParentId();
+        for (Category list : categoryList) {
+            String name = list.getName();
+            int id = list.getId();
+            List<Category> subCategoryList = categoryDao.getSubCategory(id);
+            map.put(name, subCategoryList);
+        }
+        mapApp.put(Global_Keys.CATEGORY, map);
+        return SUCCESS;
+    }
+
+    @SkipValidation
+    public String advancedSearch() {
+        advanceSerachList = new ArrayList<User>();
+        System.out.println(categoryId);
+        System.out.println(userName);
+        List<Integer> userIdList = mentorCategory.getUserIdByCategoryId(categoryId);
+        for (int userId : userIdList) {
+            User user=null;
+            if (userName == null) {
+                user = userDao.getUserAdvanceSearch(userId, "");
+            } else if (userName != null) {
+                user = userDao.getUserAdvanceSearch(userId, userName);
+            }
+
+            advanceSerachList.add(user);
+        }
+        if(advanceSerachList.size()==0){
+            addFieldError("searchKeyword","Nothing is find");
+        }
+        System.out.println(advanceSerachList.toString());
+        return SUCCESS;
+    }
+
+    @Override
+    public void setApplication(Map<String, Object> map) {
+        mapApp = map;
+    }
 
     public String getSearchKeyword() {
         return searchKeyword;
     }
+
     public void setSearchKeyword(String searchKeyword) {
         this.searchKeyword = searchKeyword;
     }
@@ -40,5 +107,37 @@ public class SearchAction extends GlobalAction {
 
     public void setUserList(List<User> userList) {
         this.userList = userList;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<User> getAdvanceSerachList() {
+        return advanceSerachList;
+    }
+
+    public void setAdvanceSerachList(List<User> advanceSerachList) {
+        this.advanceSerachList = advanceSerachList;
     }
 }
